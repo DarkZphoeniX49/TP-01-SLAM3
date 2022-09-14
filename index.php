49302471d04b90ec932d3abd36fc96a3753e26da
DarkZphoeniX49! <drattack49@gmail.com>
2022-09-14 16:59:34 +0200

    adding some content in php

--- index.php
+++ index.php
@@ -2,9 +2,22 @@
     <head>
        <meta charset="utf-8">
         <!-- importer le fichier de style -->
-        <link rel="stylesheet" href="style.css" media="screen" type="text/css" />
+        <link rel="stylesheet" href="css/main.css" media="screen" type="text/css" />
     </head>
     <body>
+<?php
+	//Exemple de syntaxe de connexion à la base de données pour PHP et MySQL.
+	
+	//Se connecter à la base de données
+	
+	session_start();
+    $servername = 'localhost';
+    $dbname='bddbdr';
+    $username = 'user2';
+    $password = '1234';
+ 
+?>
+
         <div id="container">
             <!-- zone de connexion -->
             
@@ -17,9 +30,12 @@
                 <label><b>Mot de passe</b></label>
                 <input type="password" placeholder="Entrer le mot de passe" name="password" required>
 
-                <input type="submit" id='submit' value='sinscire'>       <?php
+                <input type="submit" id='submit' value='sinscire'>       <div class="red"><?php
+                if($_SESSION["connect"]){
+                    echo 'erreur de connection !';
+                }
                 // Code de vérification 
-                ?>
+                ?></div>
             </form>
         </div>
     </body>
--- verification.php
+++ verification.php
@@ -1,12 +1,58 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-<p>vous êtes inscrit !</p>
+
+    <?php 
+
+    session_start();
+
+    try{
+            $servername = 'localhost';
+        $dbname='slamtp';
+        $username = 'user2';
+        $password = '1234';
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $_SESSION['connect']=0;
+        $_POST["username"]=$username;
+        $_POST["password"]=$password;
+
+        $_SESSION["nom"]=$username;
+        $_SESSION["mdp"]=$password;
+    
+        $req=$conn->prepare("SELECT count(*) FROM connexion where 
+        nom = '.$username.' AND  mdp='.$password.'; ");
+        $req->execute();
+        $result=$req->fetch();
+
+        
+
+        if($result !==0)
+        {
+            echo "connection réussi ! ";
+            Header("Location: menu.php");
+        }
+        else{
+            $_SESSION['connect']++;
+            Header('Location: index.php');
+        }
+
+        $conn=null;
+    }
+    catch(PDOException $e){
+        echo "Erreur : " . $e->getMessage();
+        die;
+        }
+
+?>
+    <p>vous êtes inscrit !</p>
 </body>
+
 </html>
