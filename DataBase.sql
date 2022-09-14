958d9d4e1a89c989c72bec55c4ff24354f98550e
DarkZphoeniX49! <drattack49@gmail.com>
2022-09-14 15:42:53 +0200

    update db

--- DataBase.sql
+++ DataBase.sql
@@ -1,24 +1,51 @@
-DROP DATABASE IF EXISTS MLR1;
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : mer. 14 sep. 2022 à 15:39
+-- Version du serveur : 10.4.21-MariaDB
+-- Version de PHP : 8.0.10
 
-CREATE DATABASE IF NOT EXISTS MLR1;
-USE MLR1;
-# -----------------------------------------------------------------------------
-#       TABLE : UTILISATEUR
-# -----------------------------------------------------------------------------
-
-CREATE TABLE IF NOT EXISTS UTILISATEUR
- (
-   LOGIN DB_TEXT, 32 NOT NULL  ,
-   MOT_DE_PASSE DB_TEXT, 32 NOT NULL  ,
-   DATE_CRÉATION DB_DATE(8) NOT NULL  ,
-   DATE_D_ACTIVATION DB_TEXT, 32 NULL  ,
-   ID DB_INTEGER(2) NOT NULL AUTO_INCREMENT 
-   , PRIMARY KEY (ID) 
- ) 
- comment = "";
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
 
-# -----------------------------------------------------------------------------
-#       CREATION DES REFERENCES DE TABLE
-# -----------------------------------------------------------------------------
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Base de données : `slamtp`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `login`
+--
+
+CREATE TABLE `login` (
+  `Login` varchar(250) NOT NULL,
+  `mdp` varchar(250) NOT NULL,
+  `date_crea` date NOT NULL,
+  `date_activ` date DEFAULT NULL,
+  `id` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
